d3908a2638e83d2e4e969d9a48bd3a26acbedb5d
Cyberis <christopher.bortz@gmail.com>
2026-03-23 22:46:54 -0400

    Read Text From a File

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Errorf("Could not open file 'messages.txt': %v", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 8)
+
+	for {
+		n, err := file.Read(buffer)
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			fmt.Errorf("Unknow read error: %v", err)
+			os.Exit(1)
+		}
+		fmt.Printf("read: %s\n", buffer[:n])
+	}
 }
